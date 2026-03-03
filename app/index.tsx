553eb9b8d20379ff9b3db7fc9aafb81dfce5d499
up240527-ops <up240527@alumnos.upa.edu.mx>
2026-03-02 20:12:51 -0600

    add fetch getPokemons

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  useEffect(() => {
+    console.log("entre entre en pantalla");
+    getPokemons();
+  }, []);
+  const getPokemons = async () => {
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+    const response = await fetch(URL, {
+      method: "GET",
+    });
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+  };
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text> Axel Moises</Text>
     </View>
   );
 }
